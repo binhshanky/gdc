6a5703148a0f836a3578ecfedb5b72c01992937c
Ông Thanh Bình <44429423+binhshanky@users.noreply.github.com>
2019-10-24 08:04:04 +0000

    Update SOS

--- lt.py
+++ lt.py
@@ -3,10 +3,11 @@ import time
 from time import strftime, gmtime
 import requests
 while True:
+    sos  = "" # Neu sos = "sos" thi no se gui sos cap cuu
     g = geocoder.ip('me')
     t = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     arg = g.latlng;
-    url = 'https://forgd.me/lc.php?n='+str(arg[0])+'&e='+str(arg[1])+'&lt='+t    
+    url = 'https://forgd.me/lc.php?n='+str(arg[0])+'&e='+str(arg[1])+'&lt='+t+"&sos="+sos    
     r = requests.get(url)
     if('Success' in str(r.content)):
         print('[{}] Send success. {}'.format(t,g.latlng))
